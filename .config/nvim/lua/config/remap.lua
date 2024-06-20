2d7500ff190b81fb07c3d04141ccb4db25a3c805
Ollie <github@ollie2304.co.uk>
2024-06-20 16:02:08 +0100

    update neovim, add lsp

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,4 +1,39 @@
 -- set leader key to <space>
 vim.g.mapleader = " "
+
 -- set local leader to <\\>
 vim.g.maplocalleader = "\\"
+
+-- allows moving of lines when in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- sets cursor to say in the same place when using J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- keep cursor in middle when half-page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in the middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keeps yank buffer when pasting over text
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- clear search highlight on esc
+--vim.keymap.set("n", "<Esc", "<cmd>nohlsearch<CR>")
+
+-- force use of hjkl to move
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+
+-- use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -6,8 +6,8 @@ vim.opt.relativenumber = true
 -- open horizontal splits at the bottom
 vim.opt.splitbelow = true
 -- open vertical splits to the right
-
 vim.opt.splitright = true
+
 -- disable line wrapping
 vim.opt.wrap = false
 
@@ -19,6 +19,9 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 -- insert spaces instead of tabs
 vim.opt.expandtab = true
+
+-- auto indent when starting new line
+vim.opt.smartindent = true
 
 -- sync clipboard with system clipboard
 vim.opt.clipboard = "unnamedplus"
@@ -33,8 +36,46 @@ vim.opt.virtualedit = "block"
 -- when previewing changes for text replacement show in split window
 vim.opt.inccommand = "split"
 
--- ignore case when typing commands
+-- disable highlighting on search
+vim.opt.hlsearch = false
+
+-- While typing a search command, show where the pattern, as it was typed so far, matches. 
+vim.opt.incsearch = true
+
+-- ignore case when typing commands unless \C or more caps in search term
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- enable 24-bit colour support
 vim.opt.termguicolors = true
+
+-- enable block cursor 
+vim.opt.guicursor = ""
+
+-- always show sign column
+vim.opt.signcolumn = "yes"
+
+-- if this many milliseconds nothing is typed the swap file will be written to disk
+vim.opt.updatetime = 50
+
+
+vim.opt.colorcolumn = "80"
+
+-- enable nerd font
+vim.g.have_nerd_font = true
+
+-- every wrapped line will continue visually indented
+vim.opt.breakindent = true
+
+-- save undo history
+vim.opt.undofile = true
+
+-- decrease mapped sequence wait time/ which-key popup
+vim.opt.timeoutlen = 300
+
+-- display certain whitespace chars
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- shows which line cursor is on
+vim.opt.cursorline = true
