0b54636a0cfeff3e24209d1e771cc644dcd092ea
Ollie <github@ollie2304.co.uk>
2024-06-21 08:04:43 +0100

    update neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,11 @@ return
 
     {
         "neovim/nvim-lspconfig",
+        config = function()
+            vim.keymap.set("n", "K", vim.lsp.buf.hover,{})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition,{})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,{})
+        end
     },
 
     {
@@ -24,7 +29,9 @@ return
         "williamboman/mason-lspconfig.nvim",
         dependencies = {"mason.nvim"},
         config = function()
-            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {"lua_ls"}
+            })
             require("mason-lspconfig").setup_handlers({
                 function(server_name)
                     require("lspconfig")[server_name].setup({})
